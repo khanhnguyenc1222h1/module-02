baf44d129adfa20887ce25bcbcfaa980f2c6bcf7
khanh van <vankhanhn735@gmail.com>
2023-02-28 08:49:51 +0700

    first linear equation

---
+++ [BC-JAVA-APJ] Advanced Programming with Java 2.1/lession-01/day-of-month/src/com/codegym/Main.java
@@ -0,0 +1,11 @@
+package com.codegym;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Which month that you want to count days? ");
+        int month = scanner.nextInt();
+
+    }
+}
